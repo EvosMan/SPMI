8992e551162987f2d7aef723daf71ddef6de5e9b
EvosMan <mochamadmunif71@gmail.com>
2025-06-01 14:42:39 +0700

    Update route

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,6 @@ use App\Http\Controllers\PengirimanDataController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return redirect('/login');
